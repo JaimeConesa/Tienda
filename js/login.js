01ac65a6676e86fcc8bf7ec6640ba92ec1756a15
Jaime Conesa Ponce <jaimeconesa.23@campuscamara.es>
2024-12-15 20:49:28 +0100

    a

--- js/login.js
+++ js/login.js
@@ -1,15 +1,18 @@
 // Seleccionar el formulario
-const form = document.getElementById('productForm');
+const form = document.getElementById('LoginForm');
+
 // Escuchar el evento submit
 form.addEventListener('submit', async (event) => {
     // Prevenir el envío estándar del formulario
     event.preventDefault();
+
     // Capturar los datos del formulario
     const formData = new FormData(form);
     const data = {
         usuario: formData.get('usuario'),
-        contraseña:formData.get('contraseña')
+        contraseña: formData.get('contraseña')
     };
+
     // Enviar los datos a la API RESTful
     try {
         const response = await fetch('../../php/procesar.php', {
@@ -19,14 +22,28 @@ form.addEventListener('submit', async (event) => {
             },
             body: JSON.stringify(data)
         });
+
         if (response.ok) {
             const result = await response.json();
             console.log('Inició sesión correctamente:', result);
-            alert('Inició sesión correctamente:');
+
+            // Guardar el token y la información de la tienda en el localStorage
+            if (result.token && result.tienda) {
+                localStorage.setItem('token', result.token);
+                localStorage.setItem('tienda', JSON.stringify(result.tienda));
+
+                // Redirigir al Dashboard
+                alert('Inicio de sesión exitoso. Redirigiendo al Dashboard...');
+                window.location.href = 'dashboard.html';
+            } else {
+                alert('Error: no se recibió un token o información de la tienda.');
+            }
         } else {
             console.error('Error al iniciar sesión:', response.statusText);
+            alert('Credenciales incorrectas. Intente nuevamente.');
         }
     } catch (error) {
         console.error('Error de conexión:', error);
+        alert('Error de conexión. Intente más tarde.');
     }
 });
